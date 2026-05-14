3befa4ff17cb4478fec7bf7c334fff43937d4171
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-14 15:18:42 +0200

    add express_static and move up listen
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+app.use(express.static(`${__dirname}/public`));
 
-app.listen(port, () => {
-  console.log('¡¡¡El servidor está vivo!!!');
-});
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Y el monstruo se levanta.');
+});
+
+app.listen(port, () => {
+  console.log('¡¡¡El servidor está vivo!!!');
 });
